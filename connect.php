d627fcd02c91fc8a6a5a9a5f8b5b79d698ae97e2
Dominik Łukowski <dominik1213141555@gmail.com>
2023-06-10 11:48:20 +0200

    change database connection

--- connect.php
+++ connect.php
@@ -2,6 +2,6 @@
 $host ="localhost";
 $db_user="root";
 $db_password="";
-$db_name="train_fight";
+$db_name="testbaza";
 $polaczenie =  mysqli_connect($host, $db_user, $db_password, $db_name) or die ("Błąd połączenia");
 ?>
